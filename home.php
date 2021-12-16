376707d46885ca8234b0f0ba58fa7be5e9d2ecdc
Werley0808 <96060693+Werley0808@users.noreply.github.com>
2021-12-16 14:55:57 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		Validação de formulario :)
+	</title>
+</head>
+<body>
+
+<form action="valida.php" method="post">
+	<h3>
+	<p> Nome: <input type="text" name="nome"> </p>
+	<p> Senha: <input type="password" name="senha"> </p>
+	<p> E-mail: <input type="mail" name="email"> </p>
+	<p> <button type="submit"> VALIDAR </button> </p>
+	</h3>
+</form> 
+ 
+</body>
+</html>
